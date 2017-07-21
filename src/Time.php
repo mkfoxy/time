1cf2a7ed7c5f4090339a55b3b6e53d09db5cd724
Lesnykh Ilia <lesnykh.ilja@gmail.com>
2017-07-21 17:39:45 +0300

    Add base class with few constants.

---
+++ src/Time.php
@@ -0,0 +1,22 @@
+<?php
+namespace PropellerAds\Time;
+
+/**
+ * Class Time
+ * @package PropellerAds\Time
+ */
+final class Time {
+    const SECOND = 1;
+
+    const MINUTE = 60;
+
+    const HOUR = 3600;
+
+    const DAY = 86400;
+
+    const WEEK = 604800;
+
+    const MONTH = 2592000;
+
+    const YEAR = 31536000;
+}
